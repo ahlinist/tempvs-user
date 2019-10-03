d9d0bb6f632f5d4cd7e9e822fc5d55f6875daf3c
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-10-03 20:45:53 +0300

    Add job class

---
+++ src/main/java/club/tempvs/user/job/EmailVerificationCleanupJob.java
@@ -0,0 +1,23 @@
+package club.tempvs.user.job;
+
+import club.tempvs.user.service.EmailVerificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(value = "scheduling.enabled", havingValue = "true")
+public class EmailVerificationCleanupJob {
+
+    private static final long ONE_HOUR_IN_MS = 60 * 60 * 1000;
+
+    private final EmailVerificationService emailVerificationService;
+
+    @Scheduled(fixedRate = ONE_HOUR_IN_MS, initialDelay = ONE_HOUR_IN_MS)
+    public void execute() {
+        System.out.println("Running!");
+        emailVerificationService.cleanupDayBack();
+    }
+}
